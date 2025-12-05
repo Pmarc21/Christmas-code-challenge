1339d845677602fcbe3f83c8e1f6a54c63ec509c
Pablo Barrera <pablo.marc.barrera@gmail.com>
2025-12-05 15:13:26 +0100

    reto4 done

--- 2025/reto4/reto4.py
+++ 2025/reto4/reto4.py
@@ -1,25 +1,35 @@
-santa_code = "[1++][2-][3+][<]"
-INIT_COUNTER = '['
-END_COUNTER = ']'
+santa_code = "[9+][0-][4][<]"
 def decode_santa_pin(code: str) -> str:
     last_digit = 0
     sum_total = 0
-    for char in code:
-        if char == INIT_COUNTER or char == END_COUNTER:
+    INIT_COUNTER = '['
+    END_COUNTER = ']'
+    array_numbers = []
+    code_aux = code.split(']')
+    print(code_aux)
+    for i in range(len(code_aux)):
+        if code_aux[i] == INIT_COUNTER:
             sum_total = 0
-        if char.isdigit():
-            sum_total += int(char)
-            last_digit = int(char)
+        elif code_aux[i] == END_COUNTER:
+            array_numbers.append(sum_total)
+        elif code_aux[i].isdigit():
+            sum_total += int(code_aux[i])
+            last_digit = sum_total
         else:
-            if char == '+':
+            if code_aux[i] == '+':
                 sum_total += 1
-            elif char == '-':
+                last_digit = sum_total
+            elif code_aux[i] == '-':
                 sum_total -= 1
-            else:
+                last_digit = sum_total
+            elif code_aux[i] == '<':
                 sum_total += last_digit
-        if sum_total > 10:
+        if sum_total >= 10:
             sum_total = sum_total % 10
-    final_string = "".join(str(sum_total))
-    return final_string
+    if len(array_numbers) > 3:
+        final_string = "".join(str(x) for x in array_numbers)
+        return final_string
+    else:
+        return None
 
 print(decode_santa_pin(santa_code))
